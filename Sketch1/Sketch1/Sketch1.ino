f138eed7fdc4e86d41a19a4e851bcac15afbf7b9
mstukolov <maxim.stukolov@gmail.com>
2017-03-15 17:48:15 +0300

    jihhi

---
+++ Sketch1/Sketch1/Sketch1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sketch1.ino
+ Created:	3/15/2017 5:47:46 PM
+ Author:	Maxim
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
